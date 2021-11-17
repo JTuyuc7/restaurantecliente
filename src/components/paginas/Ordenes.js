afcfab8a6f65c5ba3f65124862f4dc67196fc9de
james tuyuc <jaimetuyuc7@gmail.com>
2021-11-17 11:46:02 -0600

    Routing added and form component
    Adding routing to pages and add the form to add new item.

---
+++ src/components/paginas/Ordenes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Ordenes = () => {
+    return (  
+        <>
+            <div>
+                <h1 className="text-3xl font-light mb-4">Ordenes</h1>
+            </div>
+        </>
+    );
+}
+ 
+export default Ordenes;
